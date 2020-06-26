c002f47fe8508c95f823e8cc3aa688e002cb8423
Geoffrey Gilmore <tkdmaster123@gmail.com>
2020-06-26 13:18:02 -0700

    ci: add editorconfig for shfmt (#3)

--- scripts/entr.sh
+++ scripts/entr.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 
 if ! command -v entr &>/dev/null; then
-	echo "'entr' is not installed. Please install 'entr' via 'brew install entr' or from http://eradman.com/entrproject/"
-	exit 1
+  echo "'entr' is not installed. Please install 'entr' via 'brew install entr' or from http://eradman.com/entrproject/"
+  exit 1
 fi
 
 trap "exit" SIGINT
@@ -12,5 +12,5 @@ trap "exit" SIGINT
 LS_DHALL_SCRIPT="$(dirname "${BASH_SOURCE[0]}")/ls-dhall-files.sh"
 
 while true; do
-	"${LS_DHALL_SCRIPT}" | (entr -cd "$@" || true)
+  "${LS_DHALL_SCRIPT}" | (entr -cd "$@" || true)
 done
