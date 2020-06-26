dd0aa9425437d8ed6a9466c3a8e76eb3978d8783
Geoffrey Gilmore <tkdmaster123@gmail.com>
2020-06-26 11:57:15 -0700

    fix case sensitivity issue

--- ci/render-package.sh
+++ ci/render-package.sh
@@ -3,24 +3,4 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
 set -euxo pipefail
 
-pwd
-df -T
-# sudo apt-get install tree --yes
-# tree src/
-echo
-echo "./src ---"
-ls ./src
-echo
-echo "./src/deps ---"
-ls ./src/deps
-echo
-echo "./src/deps/k8s ---"
-ls ./src/deps/k8s
-echo
-echo "./src/deps/k8s/schemas ---"
-ls ./src/deps/k8s/schemas
-echo
-echo "cat ./src/deps/k8s/schemas/io.k8s.api.apps.v1.Daemonset.dhall ---"
-cat ./src/deps/k8s/schemas/io.k8s.api.apps.v1.Daemonset.dhall
-
 dhall --explain --file ./package.dhall >/dev/null
