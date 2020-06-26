a3537b2b5a562e431aff692fab9df538e243698b
Geoffrey Gilmore <tkdmaster123@gmail.com>
2020-06-26 11:44:05 -0700

    more debug

--- ci/render-package.sh
+++ ci/render-package.sh
@@ -7,6 +7,11 @@ ls
 pwd
 # sudo apt-get install tree --yes
 # tree src/
+
+ls ./src
+ls ./src/deps
+ls ./src/deps/k8s
+ls ./src/deps/k8s/schemas
 cat ./src/deps/k8s/schemas/io.k8s.api.apps.v1.Daemonset.dhall
 
 dhall --explain --file ./package.dhall >/dev/null
