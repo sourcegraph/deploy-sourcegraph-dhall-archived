8ef8ebf9989446311d12c0179e5222a9fb99c3eb
Geoffrey Gilmore <tkdmaster123@gmail.com>
2020-06-26 11:01:47 -0700

    try fixiing script

--- ci/render-package.sh
+++ ci/render-package.sh
@@ -3,6 +3,4 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
 set -euxo pipefail
 
-PACKAGE_FILE="${PACKAGE_FILE:-"package.dhall"}"
-
-dhall --explain --file "${PACKAGE_FILE}" >/dev/null
+dhall --explain --file ./package.dhall >/dev/null
